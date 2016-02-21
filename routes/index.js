7cfcd7bd144f88bca3af73d977d16894eb1d8e5f
Samu Karjalainen <karjalainensamu@gmail.com>
2016-02-21 03:16:49 +0200

    Added login and register routes. Minor fix to mobile carousel.

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,12 @@ router.get('/contact', function (req, res) {
   res.render('contact', { title: 'Bldr | contact' });
 });
 
+router.get('/login', function (req, res) {
+  res.render('login', { title: 'Bldr | login' });
+});
+
+router.get('/register', function (req, res) {
+  res.render('register', { title: 'Bldr | register' });
+});
+
 module.exports = router;
